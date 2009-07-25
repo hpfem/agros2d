9dfa6a6541109103dc294889199201f829bf080a
František Mach <mach.frantisek@gmail.com>
2009-07-25 10:38:11 +0200

    update installation script (debian)

--- agros2d.debian.sh
+++ agros2d.debian.sh
@@ -1,7 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-rm ../agros2d_*
-lrelease lang/*.ts
-qhelpgenerator doc/help/agros2d.qhp
-dpkg-buildpackage -sgpg -rfakeroot
-
+case "$1" in
+  -h )
+    qhelpgenerator ./doc/help/agros2d.qhp -o ./doc/help/agros2d.qch
+    ;;
+  -l )
+    lrelease ./lang/*.ts ./lang/*.qm
+    ;;
+  -c )
+    if qmake ./agros2d.pro ; then make ; fi
+    ;;
+  -p )
+    rm ../agros2d_*
+    dpkg-buildpackage -sgpg -rfakeroot
+    ;;
+  -a )
+    qhelpgenerator ./doc/help/agros2d.qhp -o ./doc/help/agros2d.qch
+    lrelease ./lang/*.ts ./lang/*.qm
+    if qmake ./agros2d.pro ; then make ; fi
+    rm ../agros2d_*
+    dpkg-buildpackage -sgpg -rfakeroot
+    ;;
+  * )
+    echo "Usage: agros2d.sh  [-h generate help files] [-l generate language files] [-c compile] [-p bild package] [-a all operations]"
+    ;;
+esac
