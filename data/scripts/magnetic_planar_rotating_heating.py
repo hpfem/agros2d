4bd6558e2eefde22ccc868fd0ef46c20fa8fc5b3
Pavel Karban <karban@ek611-karban.(none)>
2009-12-08 13:25:51 +0100

    new example

--- data/scripts/magnetic_planar_rotating_heating.py
+++ data/scripts/magnetic_planar_rotating_heating.py
@@ -47,10 +47,13 @@ addlabel(-0.0588442, 0.0380757, 0, "Cu-")
 zoombestfit()
 solve()
 
-N = 30
-step = 0.05/N
+# charts
+import pylab
 
-def graph(angle):
+def graphradius(angle):
+	N = 30
+	step = 0.05/N
+
 	r = []
 	Jvel = []
 	for i in range(1, N+1):
@@ -60,19 +63,45 @@ def graph(angle):
 		Jvel.append(point["Jiv_real"])
 	return r, Jvel
 
+def graphangle(radius):
+	N = 30.0
+	step = 360.0/N
 
-r00, Jvel00 = graph(00)
-r30, Jvel30 = graph(30)
-r60, Jvel60 = graph(60)
-r90, Jvel90 = graph(90)
+	Jvel = 0
+	for i in range(1, N+1):
+		angle = (i-1)*step
+		point = pointresult(radius*cos(angle/180.0*pi), radius*sin(angle/180.0*pi))
+		Jvel = Jvel + abs(point["Jiv_real"])
+ 	return Jvel
 
-# plot chart
-import pylab
+# radius
+r00, Jvel00 = graphradius(00)
+r30, Jvel30 = graphradius(30)
+r60, Jvel60 = graphradius(60)
+r90, Jvel90 = graphradius(90)
 
+pylab.subplot(2,1,1)
 # pylab.plot(r00, Jvel00, "k-", r30, Jvel30, "k+-", r60, Jvel60, "k--", r90, Jvel90, "ko-")
 pylab.plot(r00, Jvel00, r30, Jvel30, r60, Jvel60, r90, Jvel90)
 pylab.grid(1)
 pylab.xlabel("$r\,\mathrm{(m)}$")
 pylab.ylabel("$J\,\mathrm{(A/m^2)}$")
 pylab.legend(("00 deg.", "30 deg.", "60 deg.", "90 deg."), "upper left")
+
+# angle
+N = 20;
+r = []
+Pj = []
+for i in range(1, N):
+	R = (i-1)*0.05/N;
+	J = graphangle(R)
+	r.append(R)
+	Pj.append(J**2/sigma)
+
+pylab.subplot(2,1,2)
+pylab.plot(r, Pj)
+pylab.grid(1)
+pylab.xlabel("$r\,\mathrm{(m)}$")
+pylab.ylabel("$P_\mathrm{j}\,\mathrm{(W/m^3)}$")
+
 pylab.show()
