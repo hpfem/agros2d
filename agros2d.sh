6dad71706876a7d2813e5537e9d637d8d267fac0
Pavel Karban <pkarban@gmail.com>
2010-06-10 07:29:51 +0200

    fixes

--- agros2d.sh
+++ agros2d.sh
@@ -8,7 +8,7 @@ langPath="./lang"
 temporaryDirectory="./tmp"
 debianizedFiles="./data ./debian ./doc ./lang ./src ./src-remote ./agros2d.desktop ./agros2d.iss ./agros2d.pro ./COPYING ./functions.py ./problem-agros2d.xml ./README ./hermes2d"
 
-version="1.0"
+version="1.1"
 
 case "$1" in
 	help )
