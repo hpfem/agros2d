89d205756215c6bd665953e7243849312f6112df
Pavel Karban <pkarban@gmail.com>
2011-08-23 19:36:15 +0200

    minor fixes

--- src/collaboration.cpp
+++ src/collaboration.cpp
@@ -17,6 +17,8 @@
 // University of Nevada, Reno (UNR) and University of West Bohemia, Pilsen
 // Email: agros2d@googlegroups.com, home page: http://hpfem.org/agros2d/
 
+#include <QSvgRenderer>
+
 #include "collaboration.h"
 #include "scene.h"
 
