63f46a5c54817ee63ea5a416c22b1e561731f6d0
Pavel Karban <pkarban@gmail.com>
2012-02-18 15:24:05 +0100

    2.0 in agros2d.sh

--- agros2d.sh
+++ agros2d.sh
@@ -8,7 +8,7 @@ langPath="./lang"
 temporaryDirectory="./tmp"
 debianizedFiles="./data ./debian ./lang ./src ./hermes_common ./src-remote ./agros2d.desktop ./agros2d.iss ./agros2d.pro ./COPYING ./functions.py ./problem-agros2d.xml ./README ./hermes2d"
 
-version="1.9"
+version="2.0"
 
 case "$1" in
 	help )
