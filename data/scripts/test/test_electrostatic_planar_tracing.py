57e4d10696f175d8d091e95061480c7513521aad
Pavel Karban <pkarban@gmail.com>
2012-03-04 15:36:57 +0100

    fixes in test

--- data/scripts/test/test_electrostatic_planar_tracing.py
+++ data/scripts/test/test_electrostatic_planar_tracing.py
@@ -1,5 +1,5 @@
 # model
-newdocument("Electrostatic Osciloscope", "planar", "electrostatic", 2, 2, "disabled", 1, 1, 0, "steadystate", 1.0, 1.0, 0.0)
+newdocument("Electrostatic Osciloscope - particle tracing", "planar", "electrostatic", 2, 2, "disabled", 1, 1, 0, "steadystate", 1.0, 1.0, 0.0)
 
 # model
 newdocument("Electrostatic Osciloscope", "planar", "electrostatic", 2, 2, "disabled", 1, 1, 0, "steadystate", 1.0, 1.0, 0.0)
@@ -44,7 +44,7 @@ tracing = ParticleTracing()
 tracing.set_initial_position(0.015, 0.0)
 tracing.set_initial_velocity(3e7, 0.0)
 tracing.mass = 9.109e-31
-tracing.charge = 1.602e-19
+tracing.charge = -1.602e-19
 tracing.drag_force_density = 1.2041
 tracing.drag_force_reference_area = 0.0
 tracing.drag_force_coefficient = 0.0
@@ -63,7 +63,6 @@ print("mass = {0} kg".format(tracing.mass))
 print("charge  {0} C".format(tracing.charge))
 print("drag force: rho = {0} kg/m3, area = {1} m2, coeff = {2}".format(tracing.drag_force_density, tracing.drag_force_reference_area, tracing.drag_force_coefficient))
 print("tolerance: steps = {0}, tolerance = {1}".format(tracing.maximum_steps, tracing.tolerance))
-"""
 
 x, y, z = tracing.positions()
 vx, vy, vz = tracing.velocities()
@@ -71,5 +70,9 @@ vx, vy, vz = tracing.velocities()
 print("end position and velocity")
 print("x", x[-1], "y", y[-1], "z", z[-1])
 print("vx", vx[-1], "vy", vy[-1], "vz", vz[-1])
+"""
 
-# todo - test
+# test
+testStopY = test("end y", y[-1], 0.015709)
+
+print("Test: Electrostatic tracing - planar: " + str(testStopY))
