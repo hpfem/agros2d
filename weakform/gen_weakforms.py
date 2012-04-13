238fa6531a90dbdc08a6fc321eede98da4e1ced7
David Pánek <panek50@kte.zcu.cz>
2012-04-13 14:27:22 +0200

    progres on xml parser all modules (except rf) should work

--- weakform/gen_weakforms.py
+++ weakform/gen_weakforms.py
@@ -3,7 +3,7 @@
 from xml_parser import XmlParser
 
 # modules = ['electrostatic', 'current', 'heat', 'acoustic', 'elasticity', 'rf', 'magnetic']
-modules = ['electrostatic']
+modules = ['electrostatic', 'current', 'heat', 'acoustic', 'elasticity', 'magnetic']
 xml_parser = XmlParser(modules)
 xml_parser.process()
 #xml_parser.gen_doc()
