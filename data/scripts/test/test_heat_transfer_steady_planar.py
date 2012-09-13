d7310862600995b84fc756f385a87d4ee494881d
Pavel Karban <pkarban@gmail.com>
2012-09-13 19:12:17 +0200

    added velocity convection to axisymmetric heat transfer test in comsol

--- data/scripts/test/test_heat_transfer_steady_planar.py
+++ data/scripts/test/test_heat_transfer_steady_planar.py
@@ -20,8 +20,8 @@ heat.add_boundary("T inner", "heat_temperature", {"heat_temperature" : -15})
 heat.add_boundary("T outer", "heat_heat_flux", {"heat_heat_flux" : 0, "heat_convection_heat_transfer_coefficient" : 20, "heat_convection_external_temperature" : 20})
 heat.add_boundary("Neumann", "heat_heat_flux", {"heat_heat_flux" : 0, "heat_convection_heat_transfer_coefficient" : 0, "heat_convection_external_temperature" : 0})
 
-heat.add_material("Material 1", {"heat_conductivity" : 2, "heat_volume_heat" : 0}) 
-heat.add_material("Material 2", {"heat_conductivity" : 10, "heat_volume_heat" : 70000}) 
+heat.add_material("Material 1", {"heat_volume_heat" : 0, "heat_conductivity" : 2, "heat_density" : 7800, "heat_velocity_x" : 0.00002, "heat_velocity_y" : -0.00003, "heat_specific_heat" : 300, "heat_velocity_angular" : 0})
+heat.add_material("Material 2", {"heat_volume_heat" : 70000, "heat_conductivity" : 10, "heat_density" : 0, "heat_velocity_x" : 0, "heat_velocity_y" : 0, "heat_specific_heat" : 0, "heat_velocity_angular" : 0})
 
 # geometry
 geometry = agros2d.geometry
@@ -47,21 +47,21 @@ geometry.zoom_best_fit()
 problem.solve()
 
 # point value
-point = heat.local_values(0.086266, 0.087725)
-testT = agros2d.test("Temperature", point["T"], -4.363565)
-testG = agros2d.test("Gradient", point["G"], 155.570361)
-testGx = agros2d.test("Gradient - x", point["Gx"], -153.6640796)
-testGy = agros2d.test("Gradient - y", point["Gy"], -24.2793731)
-testF = agros2d.test("Heat flux", point["F"], 1555.703613)
-testFx = agros2d.test("Heat flux - x", point["Fx"], -1536.640796)
-testFy = agros2d.test("Heat flux - y", point["Fy"], -242.793731)
+point = heat.local_values(0.079734, 0.120078)
+testT = agros2d.test("Temperature", point["T"], 2.76619)
+testG = agros2d.test("Gradient", point["G"], 299.50258)
+testGx = agros2d.test("Gradient - x", point["Gx"], -132.7564285)
+testGy = agros2d.test("Gradient - y", point["Gy"], -268.47258)
+testF = agros2d.test("Heat flux", point["F"], 599.00516)
+testFx = agros2d.test("Heat flux - x", point["Fx"], -265.512857)
+testFy = agros2d.test("Heat flux - y", point["Fy"], -536.94516)
 
 # volume integral
 volume = heat.volume_integrals([0])
-testTavg = agros2d.test("Temperature", volume["T"], -0.023972)
+testTavg = agros2d.test("Temperature", volume["T"], 0.00335)
 
 # surface integral
 surface = heat.surface_integrals([0, 6, 7])
-testFlux = agros2d.test("Heat flux", surface["f"], -103.220778)
+testFlux = agros2d.test("Heat flux", surface["f"], -85.821798)
 
 print("Test: Heat transfer steady state - planar: " + str(testT and testG and testGx and testGy and testF and testFx and testFy and testTavg and testFlux))
