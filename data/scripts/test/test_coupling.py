a2709d9015a5d8814cddfde8615c1adf6a92857f
Pavel Kus <pavel.kus@gmail.com>
2012-09-21 20:51:11 +0200

    more coupling tests

--- data/scripts/test/test_coupling.py
+++ data/scripts/test/test_coupling.py
@@ -7,19 +7,38 @@
 
 
 # based on test_coupling_1_planar.py
-execfile("test_coup_curr_steady_lin_heat_steady_lin_elast_lin_weak_planar.py") 
-execfile("test_coup_curr_steady_lin_heat_trans_lin_elast_lin_weak_planar.py") 
-execfile("test_coup_curr_steady_nl_heat_steady_nl_elast_nl_hard_planar.py") 
-#execfile("test_coup_curr_steady_nl_heat_trans_nl_elast_nl_hard_planar.py") 
-execfile("test_coup_curr_steady_nl_heat_steady_nl_elast_nl_weak_planar.py") 
-#execfile("test_coup_curr_steady_lin_weak_with_heat_steady_nl_hard_with_elast_nl_planar.py")
-execfile("test_coup_curr_steady_nl_hard_with_heat_steady_nl_weak_with_elast_lin_planar.py")
-execfile("test_coup_curr_steady_nl_heat_trans_nl_elast_nl_weak_planar.py") 
+execfile("test_coup1_curr_steady_lin_heat_steady_lin_elast_lin_weak_planar.py") 
+execfile("test_coup1_curr_steady_lin_heat_trans_lin_elast_lin_weak_planar.py") 
+execfile("test_coup1_curr_steady_nl_heat_steady_nl_elast_nl_hard_planar.py") 
+#execfile("test_coup1_curr_steady_nl_heat_trans_nl_elast_nl_hard_planar.py") 
+execfile("test_coup1_curr_steady_nl_heat_steady_nl_elast_nl_weak_planar.py") 
+#does not converge:
+#execfile("test_coup1_curr_steady_lin_weak_with_heat_steady_nl_hard_with_elast_nl_planar.py")
+#wrong resutl:
+#execfile("test_coup1_curr_steady_lin_weak_with_heat_steady_lin_hard_with_elast_lin_planar.py")
+execfile("test_coup1_curr_steady_nl_hard_with_heat_steady_nl_weak_with_elast_lin_planar.py")
+execfile("test_coup1_curr_steady_nl_heat_trans_nl_elast_nl_weak_planar.py") 
 
 # based on test_coupling_2_axisymmetric.py
-execfile("test_coup_mag_harm_lin_heat_steady_lin_weak_axisym.py") 
-execfile("test_coup_mag_harm_nl_heat_steady_nl_weak_axisym.py") 
-#execfile("test_coup_mag_harm_nl_heat_steady_nl_hard_axisym.py") 
-execfile("test_coup_mag_harm_lin_heat_trans_lin_weak_axisym.py") 
-execfile("test_coup_mag_harm_nl_heat_trans_nl_weak_axisym.py") 
-#execfile("test_coup_mag_harm_nl_heat_trans_nl_hard_axisym.py") 
+execfile("test_coup2_mag_harm_lin_heat_steady_lin_weak_axisym.py") 
+execfile("test_coup2_mag_harm_nl_heat_steady_nl_weak_axisym.py") 
+#execfile("test_coup2_mag_harm_nl_heat_steady_nl_hard_axisym.py") 
+execfile("test_coup2_mag_harm_lin_heat_trans_lin_weak_axisym.py") 
+execfile("test_coup2_mag_harm_nl_heat_trans_nl_weak_axisym.py") 
+#execfile("test_coup2_mag_harm_nl_heat_trans_nl_hard_axisym.py") 
+
+#based on test_coupling_3_axisymmetric_nonlin.py
+execfile("test_coup3_curr_steady_lin_heat_steady_nl_elast_lin_axisym_weak.py")
+execfile("test_coup3_curr_steady_nl_heat_steady_nl_elast_nl_axisym_weak.py")
+#execfile("test_coup3_curr_steady_nl_heat_steady_nl_elast_nl_axisym_hard.py")
+execfile("test_coup3_curr_steady_lin_heat_trans_nl_elast_lin_axisym_weak.py")
+execfile("test_coup3_curr_steady_nl_heat_trans_nl_elast_nl_axisym_weak.py")
+#execfile("test_coup3_curr_steady_nl_heat_trans_nl_elast_nl_axisym_hard.py")
+#execfile("test_coup3_curr_steady_lin_weak_with_heat_steady_nl_hard_with_elast_nl_axisym.py")
+#execfile("test_coup3_curr_steady_nl_hard_with_heat_steady_nl_weak_with_elast_lin_axisym.py");
+
+#based on test_coupling_4_transient_planar.py
+execfile("test_coup4_curr_steady_lin_heat_trans_lin_planar_weak.py")
+#wrong result:
+#execfile("test_coup4_curr_steady_nl_heat_trans_nl_planar_weak.py") 
+#execfile("test_coup4_curr_steady_nl_heat_trans_nl_planar_hard.py")
