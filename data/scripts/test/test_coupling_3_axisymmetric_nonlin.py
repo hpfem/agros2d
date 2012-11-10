cec9592ddb27cbe4222cb1abaddf0ceec32ccac6
Pavel Kus <pavel.kus@gmail.com>
2012-11-10 01:00:19 +0100

    fix in hermes, transient works, but not all couplings

--- data/scripts/test/test_coupling_3_axisymmetric_nonlin.py
+++ data/scripts/test/test_coupling_3_axisymmetric_nonlin.py
@@ -33,6 +33,10 @@ heat.polynomial_order = 3
 heat.linearity_type = "newton"
 heat.nonlinear_tolerance = 0.001
 heat.nonlinear_steps = 20
+heat.damping_coeff = 1.0
+heat.automatic_damping = True
+heat.damping_number_to_increase = 1
+
 
 heat.add_boundary("300 K", "heat_temperature", {"heat_temperature" : 300})
 heat.add_boundary("Flux", "heat_heat_flux", {"heat_convection_external_temperature" : 20, "heat_convection_heat_transfer_coefficient" : 20, "heat_heat_flux" : 0, "heat_radiation_ambient_temperature" : 20, "heat_radiation_emissivity" : 0.95})
@@ -46,6 +50,9 @@ elasticity.polynomial_order = 3
 elasticity.linearity_type = "newton"
 elasticity.nonlinear_tolerance = 0.001
 elasticity.nonlinear_steps = 50
+elasticity.damping_coeff = 1.0
+elasticity.automatic_damping = True
+elasticity.damping_number_to_increase = 1
 
 elasticity.add_boundary("Fixed", "elasticity_fixed_fixed", {"elasticity_displacement_x" : 0, "elasticity_displacement_y" : 0})
 elasticity.add_boundary("Fixed free", "elasticity_fixed_free", {"elasticity_displacement_x" : 0, "elasticity_force_y" : 0})
