5c7e7efba7ede2058ccaada972dc0a1b3451e855
Pavel Karban <pkarban@gmail.com>
2013-01-24 15:05:00 +0100

    added plugins locales

---
+++ resources_source/lang/plugins_cs_CZ.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../plugins/acoustic/acoustic_force.cpp" line="71"/>
+        <location filename="../../plugins/current/current_force.cpp" line="71"/>
+        <location filename="../../plugins/elasticity/elasticity_force.cpp" line="71"/>
+        <location filename="../../plugins/electrostatic/electrostatic_force.cpp" line="71"/>
+        <location filename="../../plugins/flow/flow_force.cpp" line="71"/>
+        <location filename="../../plugins/heat/heat_force.cpp" line="71"/>
+        <location filename="../../plugins/magnetic/magnetic_force.cpp" line="71"/>
+        <location filename="../../plugins/rf/rf_force.cpp" line="71"/>
+        <source>Point [%1, %2] does not lie in any element</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
