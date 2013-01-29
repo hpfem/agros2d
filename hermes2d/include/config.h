c4e1dbe7a8028b9692439f277f3e026c6bbbedad
Pavel Kus <pavel.kus@gmail.com>
2013-01-29 15:31:46 +0100

    sync hermes: subdomeny fix

--- hermes2d/include/config.h
+++ hermes2d/include/config.h
@@ -1,2 +1,2 @@
-#define H2D_XML_SCHEMAS_DIRECTORY "/home/karban/Projects/hermes/hermes2d/xml_schemas"
-#define H2D_PRECALCULATED_FORMS_DIRECTORY "/home/karban/Projects/hermes/hermes2d/precalculated_forms"
+#define H2D_XML_SCHEMAS_DIRECTORY "/home/pkus/sources/hermes/hermes2d/xml_schemas"
+#define H2D_PRECALCULATED_FORMS_DIRECTORY "/home/pkus/sources/hermes/hermes2d/precalculated_forms"
