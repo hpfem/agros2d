c95eee0c0a8cb17b1ac68237c2f82a80bb35d7d7
Pavel Karban <pkarban@gmail.com>
2013-02-27 18:30:05 +0100

    fix isError()

--- src/scenenode.cpp
+++ src/scenenode.cpp
@@ -355,5 +355,5 @@ bool SceneNode::isOutsideArea() const
 
 bool SceneNode::isError()
 {
-    return (isLyingOnEdges() || !isConnected() || isOutsideArea() || isEndNode());
+    return (isOutsideArea() || connectedEdges().length() <= 1 || isLyingOnEdges());
 }
