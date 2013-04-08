ee417ccea3a0f3f000ab31752aa671761f3f6d22
Pavel Kus <pavel.kus@gmail.com>
2013-04-08 08:07:20 +0200

    progress dialog closes

--- agros2d-library/logview.cpp
+++ agros2d-library/logview.cpp
@@ -242,6 +242,7 @@ void LogDialog::createControls()
     QPushButton *btnClose = new QPushButton(tr("Abort"));
     connect(btnClose, SIGNAL(clicked()), Agros2D::problem(), SLOT(doAbortSolve()));
     connect(btnClose, SIGNAL(clicked()), this, SLOT(close()));
+    connect(Agros2D::problem(), SIGNAL(solved()), this, SLOT(close()));
 
     QHBoxLayout *layoutStatus = new QHBoxLayout();
     layoutStatus->addWidget(memoryLabel, 1, Qt::AlignLeft);
