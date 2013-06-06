24194c18e47d14f2a2c65bfd9886baa46bb54042
David Pánek <panek50@kte.zcu.cz>
2013-06-06 19:42:57 +0200

    added l'Hospital into rf modules

--- data/test/test_rf_tm_axisymmetric.py
+++ data/test/test_rf_tm_axisymmetric.py
@@ -11,7 +11,7 @@ problem.frequency = 1e+09
 # rf_tm
 rf_tm = a2d.field("rf_tm")
 rf_tm.analysis_type = "harmonic"
-rf_tm.number_of_refinements = 4
+rf_tm.number_of_refinements = 3
 rf_tm.polynomial_order = 3
 rf_tm.adaptivity_type = "disabled"
 rf_tm.linearity_type = "linear"
