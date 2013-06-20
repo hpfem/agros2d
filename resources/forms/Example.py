8895a0125a548ab693e577f77fa34d0f2cb50f3b
Pavel Karban <pkarban@gmail.com>
2013-06-20 09:04:22 +0200

    form example removed

--- resources/forms/Example.py
+++
@@ -1,9 +0,0 @@
-import agros2d as a2d
-
-print("{{lineEdit_text}}")
-print({{radioButton2_checked}})
-print({{checkBox_checked}})
-print("{{comboBox_text}}")
-print({{comboBox_index}})
-print({{spinBox_value}})
-print({{doubleSpinBox_value}})
