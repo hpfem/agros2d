a3a869ce01a7258c43ae17a113662e1fef3aad6c
František Mach <mach.frantisek@gmail.com>
2013-07-09 01:49:56 +0200

    basic serial calculation form (needs improvement)

---
+++ resources/examples/Examples/Magnetic Field/Magnetostatics/Electromagnetic Actuator.frm
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<problem:form xmlns:problem="XMLForm" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="XMLForm /home/fmach/Projekty/Agros2D/agros2d/resources/xsd/form_xml.xsd">
+
+  <config>
+    <item key="txtLoops" value="1"/>
+    <item key="txtMoveX" value="0.0"/>
+    <item key="txtMoveY" value="-0.01"/>
+    <item key="txtScaleX" value="0.0"/>
+    <item key="txtScaleY" value="0.0"/>
+    <item key="txtScaleFactor" value="0.0"/>
+    <item key="txtLabels" value="3"/>
+    <item key="txtNodes" value=""/>
+    <item key="txtEdges" value="14,15,16,17"/>
+    <item key="txtRotateX" value="0.0"/>
+    <item key="txtRotateY" value="0.0"/>
+    <item key="txtRotateAngle" value="0.0"/>
+    <item key="txtExpression" value="agros2d.field('magnetic').volume_integrals()['Wm']"/>
+    <item key="chkSolve" value="True"/>
+  </config>
+
+</problem:form>
---
+++ resources/forms/Serial Calculations.py
@@ -0,0 +1,58 @@
+import agros2d as a2d
+
+nodes = [{{txtNodes_text}}]
+edges = [{{txtEdges_text}}]
+labels = [{{txtLabels_text}}]
+loops = {{txtLoops_text}}
+
+move_x = {{txtMoveX_text}}
+move_y = {{txtMoveY_text}}
+
+rotate_x = {{txtRotateX_text}}
+rotate_y = {{txtRotateY_text}}
+rotate_angle = {{txtRotateAngle_text}}
+
+scale_x = {{txtScaleX_text}}
+scale_y = {{txtScaleY_text}}
+scale_factor = {{txtScaleFactor_text}}
+
+problem = a2d.problem()
+geometry = a2d.geometry
+
+def transform():
+  if (move_x != 0.0 or move_y != 0.0):
+    geometry.move_selection(move_x, move_y)
+
+  if (rotate_angle != 0.0):
+    geometry.rotate_selection(rotate_x, rotate_y, rotate_angle)
+
+  if (scale_factor != 0.0):
+    geometry.scale_selection(scale_x, scale_y, scale_factor)
+
+  geometry.select_none()
+
+def calculate():
+  if ({{chkSolve_checked}}):
+    problem.solve()
+
+  return {{txtExpression_text}}
+
+results = list()
+results.append(calculate())
+
+for loop in range(loops):
+  if (nodes):
+    geometry.select_nodes(nodes)
+    transform()
+
+  if (edges):
+    geometry.select_edges(edges)
+    transform()
+
+  if (labels):
+    geometry.select_labels(labels)
+    transform()
+
+  results.append(calculate())
+
+print(results)
