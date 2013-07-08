e028003698b75b47775f996fea80b3022073c087
Pavel Karban <pkarban@gmail.com>
2013-07-08 22:36:27 +0200

    added Example.ui

---
+++ resources/forms/Example.py
@@ -0,0 +1,15 @@
+import agros2d as a2d
+
+# basic controls
+print("lineEdit_text = {{lineEdit_text}}")
+print("radioButton1_checked = {{radioButton1_checked}}")
+print("radioButton2_checked = {{radioButton2_checked}}")
+print("checkBox_checked = {{checkBox_checked}}")
+print("comboBox_text = {{comboBox_text}}")
+print("comboBox_index = {{comboBox_index}}")
+print("spinBox_value = {{spinBox_value}}")
+print("doubleSpinBox_value = {{doubleSpinBox_value}}")
+
+print("lstNodes_list = {{lstNodes_list}}")
+print("lstEdges_list = {{lstEdges_list}}")
+print("lstLabels_list = {{lstLabels_list}}")
