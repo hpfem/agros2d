feafebd98920907ae828858db4067bd9305eeb4c
Pavel Karban <pkarban@gmail.com>
2013-07-25 18:58:49 +0200

    added cython to agros2d.sh

--- agros2d.sh
+++ agros2d.sh
@@ -50,6 +50,10 @@ case "$1" in
     equations )
         find ./plugins/ -name "*.py" -exec python {} \;
         ;;       
+    cython )
+        cython --cplus resources_source/python/pythonlab.pyx
+        cython --cplus resources_source/python/agros2d.pyx
+        ;;       
     build )
         case "$2" in
             binary )
@@ -92,6 +96,6 @@ case "$1" in
         esac
         ;;
     * )
-        echo "Usage: agros2d.sh\n\t [equations - build equations]\n\t [help - build and generate documentation]\n\t [lang release - release language files]\n\t [lang update - update language files]\n\t [comp - compile]\n\t [run - run]\n\t [build binary - build binary package]\n\t [build source - build source package]"
+        echo "Usage: agros2d.sh\n\t [equations - build equations]\n\t [help - build and generate documentation]\n\t [cython - run cython]\n\t [lang release - release language files]\n\t [lang update - update language files]\n\t [comp - compile]\n\t [run - run]\n\t [build binary - build binary package]\n\t [build source - build source package]"
         ;;
 esac
