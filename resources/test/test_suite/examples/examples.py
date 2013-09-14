31ad7a124044fedbe2162a202f697e45d3253807
František Mach <mach.frantisek@gmail.com>
2013-09-14 17:15:21 +0200

    revised automatic test of examples (every example has own method)

--- resources/test/test_suite/examples/examples.py
+++ resources/test/test_suite/examples/examples.py
@@ -5,42 +5,41 @@ import os
 from test_suite.scenario import Agros2DTestCase
 from test_suite.scenario import Agros2DTestResult
 
-class ExamplesTest(Agros2DTestCase):
-    def getExamples(self, dir):
-        self.a2d_examples = list()
-        self.py_examples = list()
+def create_tests(case, dir):
+    for (path, dirs, files) in os.walk(dir):
+        for file in files:
+            name, extension = os.path.splitext(file)
 
-        for (path, dirs, files) in os.walk(dir):
-            for file in files:
-                name, extension = os.path.splitext(file)
-                if(extension == '.a2d'):
-                    self.a2d_examples.append('{0}/{1}'.format(path, file))
-                elif(extension == '.py'):
-                    self.py_examples.append('{0}/{1}'.format(path, file))
+            method = 'test_{0}_{1}'.format(os.path.split(path)[-1].replace(" ", "_"), name.replace(" ", "_")).lower()
+            example = '{0}/{1}'.format(path, file)
 
+            if(extension == '.a2d'):
+                setattr(case, method, get_a2d_test(example))
+            elif(extension == '.py'):
+                setattr(case, method, get_py_test(example))
+
+def get_a2d_test(example):
     def test(self):
-        for example in self.a2d_examples:
-            agros2d.open_file(example)
-            agros2d.problem().solve()
+        agros2d.open_file(example)
+        agros2d.problem().solve()
+    return test
+        
+def get_py_test(example):
+    def test(self):
+        execfile(example)
+    return test
 
-        for example in self.py_examples:
-            execfile(example)
+class Examples(Agros2DTestCase): pass
+create_tests(Examples, pythonlab.datadir('/resources/examples/Examples'))
 
-class Examples(ExamplesTest):
-    def setUp(self):
-        self.getExamples(pythonlab.datadir('/resources/examples/Examples'))
+class Other(Agros2DTestCase): pass
+create_tests(Other, pythonlab.datadir('/resources/examples/Other'))
 
-class Other(ExamplesTest):
-    def setUp(self):
-        self.getExamples(pythonlab.datadir('/resources/examples/Other'))
+class PythonLab(Agros2DTestCase): pass
+create_tests(PythonLab, pythonlab.datadir('/resources/examples/PythonLab'))
 
-class PythonLab(ExamplesTest):
-    def setUp(self):
-        self.getExamples(pythonlab.datadir('/resources/examples/PythonLab'))
-
-class Tutorials(ExamplesTest):
-    def setUp(self):
-        self.getExamples(pythonlab.datadir('/resources/examples/Tutorials'))
+class Tutorials(Agros2DTestCase): pass
+create_tests(Tutorials, pythonlab.datadir('/resources/examples/Tutorials'))
 
 if __name__ == '__main__':        
     import unittest as ut
