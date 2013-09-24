a3d131047781d1621a894ab08d72f44498a20ff5
Pavel Karban <pkarban@gmail.com>
2013-09-24 17:45:19 +0200

    improved transient example

--- resources/examples/Other/Agros2D/Control of Transient Process.py
+++ resources/examples/Other/Agros2D/Control of Transient Process.py
@@ -1,6 +1,13 @@
 import agros2d as a2d
+import pythonlab
+import pylab as pl
 
-TEMP_COIL = 330
+TEMP_COIL = 360
+SIGMA = 57e6
+N = 1500
+k = 0.65
+S = 1.943e-03
+IMAX = 3.0
 
 def time_callback(time_step):
     heat = a2d.field("heat")    
@@ -9,11 +16,30 @@ def time_callback(time_step):
     time = problem.time_steps_total()[time_step]
     avg = volume["T"] / volume["V"]
     print('time = {0} s, average coil temperature = {1} K'.format(time, avg))
+
+    # current
     if (avg > TEMP_COIL):
-        print('Average coil temperature reached {0} K'.format(TEMP_COIL))
-        return False
+        I = 0.0
+    else:
+        I = IMAX
+    
+    # current density
+    J = N*I/(S*k)
+    # Joule losses
+    pj = (J**2)/SIGMA
+            
+    # set source
+    heat.modify_material("Copper", { "heat_volume_heat" : pj })
+
+    total_time.append(time)
+    temp.append(avg)
+    current.append(I)
 
     return True
+
+temp = [] 
+current = []
+total_time = []
 
 # problem
 problem = a2d.problem(clear = True)
@@ -21,8 +47,8 @@ problem.coordinate_type = "axisymmetric"
 problem.mesh_type = "triangle"
 problem.time_step_method = "fixed"
 problem.time_method_order = 2
-problem.time_total = 1500
-problem.time_steps = 5
+problem.time_total = 2500
+problem.time_steps = 40
 problem.time_callback = time_callback
 
 # heat
@@ -80,6 +106,32 @@ geometry.add_label(0.00316251, 0.011224, materials = {"heat" : "Iron"})
 geometry.add_label(0.0163723, 0.144289, materials = {"heat" : "Copper"})
 geometry.add_label(0.00380689, 0.151055, materials = {"heat" : "Air"})
 geometry.add_label(0.0112064, 0.0336487, materials = {"heat" : "Brass"})
-a2d.view.zoom_best_fit()
 
+a2d.view.zoom_best_fit()
 problem.solve()
+
+# current
+pl.figure(figsize=[8,3])
+pl.bar(total_time, current, width = problem.time_total / problem.time_steps)
+pl.xlabel("time (s)")
+pl.ylabel("current (A)")
+pl.grid(True)
+
+chart_current = pythonlab.tempname("png")
+pl.savefig(chart_current, dpi=60)
+pl.close()
+
+pythonlab.image(chart_current)
+
+# temperature
+pl.figure(figsize=[8,3])
+pl.plot(total_time, temp, '-')
+pl.xlabel("time (s)")
+pl.ylabel("temperature (K)")
+pl.grid(True)
+
+chart_temp = pythonlab.tempname("png")
+pl.savefig(chart_temp, dpi=60)
+pl.close()
+
+pythonlab.image(chart_temp)
