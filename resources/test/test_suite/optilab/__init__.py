e0828bb1b2419fcf8df67445744a935e8e060b10
František Mach <mach.frantisek@gmail.com>
2014-09-06 16:23:23 +0200

    new test added to scenarios

--- resources/test/test_suite/optilab/__init__.py
+++ resources/test/test_suite/optilab/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ["model", "model_dictionary", "model_generator", "optimization"]
+__all__ = ["model", "model_dictionary", "model_generator", "model_fiter", "model_postprocessor", "optimization"]
 
 from test_suite.optilab import model
 from test_suite.optilab import model_dictionary
 from test_suite.optilab import model_generator
+from test_suite.optilab import model_filter
+from test_suite.optilab import model_postprocessor
 from test_suite.optilab import optimization
